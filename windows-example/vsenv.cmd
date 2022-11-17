17bfddb23776be3868270fb2216ec4ce7f3e251f
tinkernels <don.johnny.cn@gmail.com>
2022-11-17 09:16:29 +0800

    Add windows example.

---
+++ windows-example/vsenv.cmd
@@ -0,0 +1,21 @@
+@echo off
+chcp 65001 >NUL 2>NUL
+rem
+rem Uncomment setting VSCMD_DEBUG to enable debugging to output
+rem
+rem set VSCMD_DEBUG=3
+
+rem
+rem   Determine path to VsDevCmd.bat
+rem
+for /f "usebackq delims=#" %%a in (`"%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -latest -property installationPath`) do set found_vcvar_bat_path_by_me=%%a\VC\Auxiliary\Build
+
+if [%1] equ [64] (
+    call "%found_vcvar_bat_path_by_me%\vcvars64.bat"
+) else if [%1] equ [32] (
+    call "%found_vcvar_bat_path_by_me%\vcvars32.bat"
+) else (
+    call "%found_vcvar_bat_path_by_me%\vcvarsall.bat" %*
+)
+
+set found_vcvar_bat_path_by_me=
